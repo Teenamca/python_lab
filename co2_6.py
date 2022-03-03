a7a886c4a812cca0ae6535e9994ba67112efe660
Teenamca <94525853+Teenamca@users.noreply.github.com>
2022-03-03 20:46:13 +0530

    Add files via upload

---
+++ co2_6.py
@@ -0,0 +1,11 @@
+def char_frequency(str1):
+    dict = {}
+    for n in str1:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+a=input("Enter the string ")
+print(char_frequency(a))
