9d752ca72817f07454d773f2f9115cf386d4b766
Teenamca <94525853+Teenamca@users.noreply.github.com>
2022-03-08 12:54:40 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,15 @@
+def compare(n):
+    n = str(0)
+    n= str(0)
+    for i in range(1):
+        if(str(S1)==str(S2)):
+            print("True")
+        return True
+    else:
+        print("False")
+    return False
+S1=input("Enter the first string")
+S2=input("Enter the second string")
+print("The first string is :",S1)
+print("The second string is :",S2)
+
