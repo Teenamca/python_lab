3bccd60298d37d3bb35c733267048f4e54488c42
Teenamca <94525853+Teenamca@users.noreply.github.com>
2022-03-08 12:53:46 +0530

    Add files via upload

---
+++ areas.py
@@ -0,0 +1,13 @@
+import math
+from math import pi
+b=float(input("Enter the base of triangle :"))
+h=float(input("Enter the height of the triangle :"))
+t_calc=0.5*b*h
+print("Area of triangle is :",t_calc)
+l=float(input("Enter the length of rectangle :"))
+b=float(input("Enter the breadth of rectangle :"))
+r_calc=l*b
+print("Area of rectangle",r_calc)
+r=float(input("Enter the radius of circle :"))
+a_calc=pi*r**r
+print("Area of the circle",a_calc)
